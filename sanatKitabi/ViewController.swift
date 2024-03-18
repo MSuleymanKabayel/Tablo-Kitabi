428d6f27d263004473e83444ab681aaf9a623501
MSuleymanKabayel <msuleymankabayel@gmail.com>
2024-03-18 14:10:44 +0300

    Initial Commit

---
+++ sanatKitabi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sanatKitabi
+//
+//  Created by Süleyman Kabayel on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
